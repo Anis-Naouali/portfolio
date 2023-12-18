b4046bded4a592baabd42f03c176569780797a0e
Anis-Naouali <anis.naouali@esprit.tn>
2023-12-18 23:05:25 +0100

    update the header and make the profil page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,16 +3,21 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Signin from "./pages/Signin";
 import SignUp from "./pages/SignUp";
+import Profil from "./pages/Profil";
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 export default function App() {
   return (
     <BrowserRouter>
-    <Header/>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profil" element={<Profil />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);  console.log(currentUser);
+
   return (
     <div>
       <header className="bg-slate-300">
         <div className="justify-between gap-4 items-center flex max-w-6xl mx-auto p-1.5 sm:p-3">
           <Link to="/">
-            <span className="font-bold text-sm sm:text-xl ">
-              Anis Naouali
-            </span>
+            <span className="font-bold text-sm sm:text-xl ">Anis Naouali</span>
           </Link>
           <ul className="flex gap-4">
             <Link to="/">
@@ -16,9 +17,17 @@ export default function Header() {
             <Link to="/about">
               <li className="  sm:inline hover:underline">About</li>
             </Link>
-            <Link to="/signin">
-              <li className=" sm:inline hover:underline">Sign In</li>
-            </Link>
+            <Link to='/profil'>
+            {currentUser ? (
+             <img
+             className='rounded-full h-7 w-7 object-cover'
+             src={currentUser.avatar}
+             alt='profile'
+           />
+            ) : (
+              <li className=' text-slate-700 hover:underline'> Sign in</li>
+            )}
+          </Link>
           </ul>
         </div>
       </header>
