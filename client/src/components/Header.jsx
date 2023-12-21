70b0ee6ee9cb009bbe488e2164b7957c755e937a
Anis-Naouali <anis.naouali@esprit.tn>
2023-12-22 00:29:28 +0100

    image upload fonctionnality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 export default function Header() {
-  const { currentUser } = useSelector((state) => state.user);  console.log(currentUser);
+  const { currentUser } = useSelector((state) => state.user); 
 
   return (
     <div>
